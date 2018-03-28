ddbba5c6f9aefcb5fae18d422e7a7cdcec59881e
KingNate51 <kingnate51@yahoo.com>
2018-03-28 16:25:53 -0700

    Background Tasks

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,9 @@ from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 from elasticsearch import Elasticsearch
 from config import Config
+from redis import Redis
+import rq
+
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -37,6 +40,9 @@ def create_app(config_class=Config):
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
 
+    app.redis = Redis.from_url(app.config['REDIS_URL'])
+    app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
+
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
 
@@ -45,6 +51,7 @@ def create_app(config_class=Config):
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
 
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
